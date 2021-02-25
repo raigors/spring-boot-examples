2dbff63868dfe361b6161b804dc132e6ca303c0a
shishaodong <spring-boot@qq.com>
2021-02-25 22:26:02 +0800

    actuator demo

---
+++ spring-boot-actuator/src/main/java/com/github/spring/boot/actuator/controller/ActuatorController.java
@@ -0,0 +1,26 @@
+package com.github.spring.boot.actuator.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 创建时间为 15:38-2019-03-21
+ * 项目名称 SpringBootActuator
+ * </p>
+ *
+ * @author shao
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+
+
+@RestController
+public class ActuatorController {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "hello";
+    }
+
+}
