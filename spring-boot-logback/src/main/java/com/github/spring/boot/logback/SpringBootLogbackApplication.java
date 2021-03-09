6c89b6097db25520831106e737e713a43641de8c
shishaodong <spring-boot@qq.com>
2021-03-09 13:37:27 +0800

    日志工程

--- spring-boot-logback/src/main/java/com/github/spring/boot/logback/SpringBootLogbackApplication.java
+++ spring-boot-logback/src/main/java/com/github/spring/boot/logback/SpringBootLogbackApplication.java
@@ -2,7 +2,12 @@ package com.github.spring.boot.logback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * @author shishaodong
+ */
+@EnableScheduling
 @SpringBootApplication
 public class SpringBootLogbackApplication {
 
---
+++ spring-boot-logback/src/main/java/com/github/spring/boot/logback/schedule/ScheduleTrace.java
@@ -0,0 +1,25 @@
+package com.github.spring.boot.logback.schedule;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * TODO
+ * <p>
+ * create in 2021/3/8 3:10 下午
+ *
+ * @author shishaodong
+ * @version 0.0.1
+ */
+@Slf4j
+@Component
+public class ScheduleTrace {
+
+    @Scheduled(fixedRate = 1500L)
+    public void task() throws InterruptedException {
+        log.trace("trace");
+//        TimeUnit.SECONDS.sleep(10);
+    }
+
+}
