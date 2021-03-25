c2ffc1c997a5fdc11efc23223081a2e2f88a53b5
shishaodong <spring-boot@qq.com>
2021-03-25 19:58:28 +0800

    update modules

---
+++ spring-boot-zookeeper/src/main/java/com/github/spring/boot/zookeeper/SpringBootZookeeperApplication.java
@@ -0,0 +1,13 @@
+package com.github.spring.boot.zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootZookeeperApplication.class, args);
+    }
+
+}
---
+++ spring-boot-zookeeper/src/test/java/com/github/spring/boot/zookeeper/SpringBootZookeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.spring.boot.zookeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootZookeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
